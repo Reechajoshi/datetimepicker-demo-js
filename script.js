a3d2724afb56cbffaa878b857f42c3eae2f1084c
reechajoshi <joshi.richa.ramesh@gmail.com>
2018-11-07 14:56:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+  jQuery Document ready
+*/
+$(function()
+{
+	$('#basic_example_1').datetimepicker();
+	
+	/*
+		below code just enable time picker.
+	*/	
+	$('#basic_example_2').timepicker();
+});
